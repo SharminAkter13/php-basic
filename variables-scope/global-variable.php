79a35ccd111ed0b3eb74019c8ef11b24a0618637
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-21 13:18:05 +0600

    new

--- variables-scope/global-variable.php
+++ variables-scope/global-variable.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>global variables</title>
 </head>
 <body>
+    <h1>global variables</h1>
+
+    <?php
+
+    $x =56;
+    $y =5;
+    function  sub(){
+        global $x, $y, $z;
+        $z =$x-$y;
+    }
+    sub();
+    echo $z;
     
+    ?>
 </body>
 </html>
