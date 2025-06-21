6a77ebbe1e1406f9f84140e3dbf1e5dae3bc6d3c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-21 12:21:08 +0600

    new

---
+++ variables-scope/local-variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>local variables</title>
+</head>
+<body>
+    <h1>local variables</h1>
+
+    <?php
+
+    function data(){
+        $x =25;
+        echo $x;
+    }
+    
+    data();
+    ?>
+</body>
+</html>
--- variables-scope/super-global.php
+++ variables-scope/super-global.php
@@ -6,15 +6,20 @@
     <title>super global variables</title>
 </head>
 <body>
-    <h1>super global variables</h1>
-    
+
+
+<h1>super global variables</h1>
     <?php
     echo $_SERVER['SERVER_NAME'];
-    echo "<br>";
-    echo $_SERVER['SERVER_PORT'];    
-    echo "<br>";
+    echo"<br>";
+    echo $_SERVER['SERVER_PORT'];
+    echo"<br>";
+    echo $_SERVER['SERVER_ADDR'];
+    echo"<br>";
     echo $_SERVER['SCRIPT_FILENAME'];
     
+    
     ?>
+    
 </body>
 </html>
