7473cb874b2cc32dabb48261d25197c3eb26a0dc
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-21 10:37:12 +0600

    new

---
+++ data-type/integer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data type - integer</title>
+</head>
+<body>
+    <?php
+    $number = 1995;
+    var_dump($number);
+    
+    ?>
+</body>
+</html>
