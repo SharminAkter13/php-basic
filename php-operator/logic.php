b52b8282116819f06e4993b586f7fdad00867e99
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-23 01:26:27 +0600

    new

--- php-operator/logic.php
+++ php-operator/logic.php
@@ -17,5 +17,18 @@
     var_dump($c);
     var_dump($d);
     ?>
+    <br>
+     <?php
+    $a =!( false xor true);
+    $b = false xor true;
+    $c = !(true xor true);
+    $d = true xor true;
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
+    var_dump($d);
+    ?>
+
+
 </body>
 </html>
