9e8d2e0566dc28d1cecc1a2b18641bc2c1abfce5
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-25 13:01:43 +0600

    neew

---
+++ array_function/array_unshift .php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array shift Function</title>
+</head>
+<body>
+    <!-- Removes the first element from an array, and returns the value of the removed element -->
+<h3>Array shift Function</h3>
+    <?php
+    $input = array("a"=>"banana","b"=>"apple","c"=>"orange");
+    array_shift($input,"mango");
+    print_r($input);
+    ?>
+</body>
+</html>
