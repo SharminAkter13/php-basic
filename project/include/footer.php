6f0945c771f67e601b44124b222677b49edd39e8
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-26 13:02:45 +0600

    new

---
+++ project/include/footer.php
@@ -0,0 +1,152 @@
+<footer class="bg-dark text-light ">
+    <div class="container">
+      <div class="row ">
+        <div class="col-md-2 mb-4 mb-md-0 me-lg-4 ms-lg-2 mt-4">
+          <h6 class="text-uppercase fw-bold mb-3">RACING</h6>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-decoration-none text-light">Scuderia Ferrari HP</a></li>
+            <li><a href="#" class="text-decoration-none text-light">charles Leclerc</a></li>
+            <li><a href="#" class="text-decoration-none text-light">lewis hamilton</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Hypercar</a></li>
+            <li><a href="#" class="text-decoration-none text-light">GT Series</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Esports</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Scuderia Ferrari Driver Academy</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Scuderia Ferrari Member</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 mb-4 mb-md-0 me-lg-4 mt-4">
+          <h6 class="text-uppercase fw-bold mb-3">SPORTS CARS</h6>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-decoration-none text-light">Range</a></li>
+            <li><a href="#" class="text-decoration-none text-light"> Configure your Ferrari</a></li>
+            <li><a href="#" class="text-decoration-none text-light">MyFerrari</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Pre-owned</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Dealers</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Recall information</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Techinfo</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 mb-4 mb-md-0 me-lg-4 mt-4">
+          <h6 class="text-uppercase fw-bold mb-3">COLLECTIONS</h6>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-decoration-none text-light">Men</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Women</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Kids</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Shoes</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Eyewear</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Collectibles</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Scuderia Ferrari Selection</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 mb-4 mb-md-0 me-lg-4 mt-4">
+          <h6 class="text-uppercase fw-bold mb-3">EXPERIENCES</h6>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-decoration-none text-light"> Corse Clienti</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Ferrari Race Series</a></li>
+            <li><a href="#" class="text-decoration-none text-light"> Ristorante cavallino</a></li>
+            <li><a href="#" class="text-decoration-none text-light"> Ferrari Museums</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Ferrari World Abu Dhabi</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Ferrari Land Barcelona</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 mb-4 mb-md-0 me-lg-4 mt-4">
+          <h6 class="text-uppercase fw-bold mb-3">ABOUT US</h6>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-decoration-none text-light">Corporate</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Sustainability</a></li>
+            <li><a href="#" class="text-decoration-none text-light"> Media Centre</a></li>
+            <li><a href="#" class="text-decoration-none text-light">News</a></li>
+            <li><a href="#" class="text-decoration-none text-light">Magazine</a></li>
+            <li><a href="#" class="text-decoration-none text-light">History</a></li>
+            <li><a href="#" class="text-decoration-none text-light">join us</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="text-light my-2 ">
+      <div class="row text-center justify-content-center">
+        <div class="col-md-7 justify-content-center align-items-center w-100 ">
+          <ul class="list-inline justify-content-center align-content-center ">
+            <li class="list-inline-item me-lg-5 p-lg-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-facebook text-white"></i> FACEBOOK</a></li>
+            <li class="list-inline-item "><span class="text-light">+</span></li>
+            <li class="list-inline-item me-lg-5 p-lg-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-instagram  text-white"></i> INSTAGRAM</a></li>
+            <li class="list-inline-item"><span class="text-light">+</span></li>
+            <li class="list-inline-item me-lg-5 p-lg-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-linkedin text-white"></i> LINKEDIN</a></li>
+            <li class="list-inline-item"><span class="text-light">+</span></li>
+            <li class="list-inline-item me-md-5 p-md-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-tiktok text-white"></i> TIK TOK</a></li>
+                  <li class="list-inline-item me-lg-5 p-lg-4"><a href="#" class="text-decoration-none text-light"><i
+                    class="bi bi-twitch text-white"></i> TWITCH</a></li>
+              <li class="list-inline-item "><span class="text-light">+</span></li>
+            <li class="list-inline-item me-md-5 p-md-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-twitter-x text-white"></i> X</a></li>
+            <li class="list-inline-item"><span class="text-light">+</span></li>
+            <li class="list-inline-item me-md-5 p-md-4"><a href="#" class="text-decoration-none text-light"><i
+                  class="bi bi-youtube text-white"></i> YOUTUBE</a></li>
+            <li class="list-inline-item"><span class="text-light">+</span></li>
+
+          </ul>
+        </div>
+      </div>
+      <hr class="text-light my-2 ">
+      <div class="row text-center mt-lg-3">
+        <p>
+          Ferrari N.V. - Holding company - A company under Dutch law, having its official seat in Amsterdam, the
+          Netherlands and its corporate address at Via Abetone Inferiore No. 4, I-41053 Maranello (MO), Italy,
+          registered with the Dutch trade register under number 64060977
+        </p>
+        <p>
+          Ferrari S.p.A. - A company under Italian law, having its registered office at Via Emilia Est No. 1163, Modena,
+          Italy, Companies Register of Modena, VAT and Tax number 00159560366 and share capital of Euro 20,260,000
+        </p>
+        <p>
+          Copyright 2025 - All rights reserved
+        </p>
+      </div>
+    </div>
+ 
+  <div class="bg-secondary py-lg-2 align-content-lg-start" style="background-color: #353738 !important;">
+    <div class="container">
+      <div class="row justify-content-between align-items-lg-start f-1">
+        <div class="col-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small">Legal</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Privacy
+                Policy</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Cookie
+                Policy</a></li>
+            <li class="list-inline-item"><a href="#"
+                class="text-light text-decoration-none small ms-3">Accessibility</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Submit your
+                privacy request</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Contacts</a>
+            </li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Corporate
+                ENG</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Corporate
+                ITA</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small">Responsible
+                Disclosure</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Reward
+                Anticounterfeiting</a></li>
+            <li class="list-inline-item"><a href="#" class="text-light text-decoration-none small ms-3">Media Centre</a>
+            </li>
+          </ul>
+        </div>
+       
+      </div>
+      </div>
+      
+    </div>
+
+  </div>
+<div class="bg-dark text-center footer-logo ">
+    <div class="container">
+        <img src="dist/images/shell-logo-png-transparent.png" alt="Shell Logo" class="img-fluid" style="max-width: 100px; height: 40px;">
+    </div>
+</div>
+  
+
+ </footer>
