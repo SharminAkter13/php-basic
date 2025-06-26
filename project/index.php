7c1a793279680f52b7e7d205872b7d5dd520c950
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-26 12:54:17 +0600

    new

--- project/index.php
+++ project/index.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Official Ferrari Website</title>
-  <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="dist/vendor/animate.css/animate.min.css">
-  <link href="dist/images/d-logo.png" rel="icon" type="image/x-icon" sizes="32*38">
-  <link href="dist/css/bootstrap.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" >
-
-  <!-- external CSS -->
-  <link rel="stylesheet" href="style.css" />
-</head>
-
-<body>
+<!-- header start -->
+<?php
+    include("include/header.php");
+  ?>
+<!-- header end -->
   <!-- NAVBAR start -->
   <?php
-    include(include/nav.php);
+    include("include/nav.php");
   ?>
   
 
