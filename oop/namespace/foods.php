698d856b9119ab238331afe861a90ba51c6429df
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-29 23:37:26 +0600

    new

---
+++ oop/namespace/foods.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Ali;
+class Car { 
+    public function info(){ 
+        echo "This is Car.";
+    }
+}
+
+
+?>
---
+++ oop/namespace/snacks.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Razib;
+
+class User2{
+    public $fname;
+    public $lname;
+    public function display () { 
+        echo "This is loging page";
+    }
+}
+?>
