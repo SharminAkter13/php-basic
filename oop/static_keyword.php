0ba052f02fab6cdd8961e8ecc1e1e5fd9a00e8ce
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-01 11:18:51 +0600

    new

--- oop/static_keyword.php
+++ oop/static_keyword.php
@@ -1,7 +1,12 @@
 <?php
 echo "<h3>Static Keyword </h3>";
 
-// Static properties and methods can be used without creating an instance of the class.
+/*Static properties and methods can be used without creating an instance of the class.
+
+    Syntax for Static Properties: ClassName::$propertyName;
+
+    Syntax for Static Methods: ClassName::methodName();
+*/
 class Trainee{
     const name ="Welcome to IDB!!! "."<br>";
 
--- oop/static_method_property.php
+++ oop/static_method_property.php
@@ -6,17 +6,34 @@ class Student{
         return self::$reg;
     }
 
-    public function view(){
+    public static  function view(){
       return "This page Show Static Method";
 }
 
-public function ssValue(){
+public static function ssValue(){
     return self::view();
 }
 }
-$details = new Student();
-echo $details->studentValue();
-// echo Student::studentValue();
-echo $details->view();
+
+class StudentDetails extends Student{
+    public function sdValue(){
+        return parent::$reg;
+    }
+    private function view2(){
+        return "This page Show Static Method of child class";
+  }
+  
+  public function sdValue2(){
+      return self::view2();
+  }
+  
+}
+
+
+$details = new StudentDetails();
+echo $details->sdValue();
+echo "<br>";
+// echo Student::studentValue(); //it will show error cause the function is not static
+echo StudentDetails::ssValue();
 
 ?>
