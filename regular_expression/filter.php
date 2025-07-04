330b17a13ada1e8f29cb5280a991bfc01625dfa1
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-04 22:53:18 +0600

    regex

--- regular_expression/filter.php
+++ regular_expression/filter.php
@@ -1,6 +1,7 @@
 <?php
 // preg_filter() work like preg_replace(), but it returns only the elements where replacements happened.
 // preg_filter() removes elements where no matches are found.
+// The preg_filter() function returns a string or array of strings in which matches of the pattern have been replaced with the replacement string.
 $in =[
     'KHLIFA is My Favourit Book',
     'MISSION IMPOSSIBLE is My Favourit Movie',
