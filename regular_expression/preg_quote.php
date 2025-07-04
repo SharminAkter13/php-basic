2762fae36b8b84a9d239269cab7c8d36e6b67be6
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-04 13:11:53 +0530

    regex

--- regular_expression/preg_quote.php
+++ regular_expression/preg_quote.php
@@ -1,6 +1,5 @@
 <?php
-
-echo preg_quote("This is a $ text. this is another");
-
-//This is a \$ text\.
+// Use preg_quote() to safely use special characters in a regular expression
+// adds a backslash to characters that have a special meaning in regular expressions 
+echo preg_quote("This is $ Life.");
 ?>
