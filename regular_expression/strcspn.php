b511c2501793395dff80866117302a3d73353c7d
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-04 23:01:23 +0600

    regex

--- regular_expression/strcspn.php
+++ regular_expression/strcspn.php
@@ -1,10 +1,8 @@
 <?php
+// The strcspn() function returns the number of characters (including whitespaces) found in a string before any part of the specified characters are found.
 
-$p="asdf123456";
-
-$r=strcspn($p,"123456789");
-
-	echo $r;
-
+$t ="adbhsjh1244";
+$s =strcspn($t,"1244");
+echo $s;
 
 ?>
