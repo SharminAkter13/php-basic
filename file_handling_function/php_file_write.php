f99d2476d73c2523a9f9175ab5dbb2e6db0bcb11
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-05 13:08:53 +0600

    file handling

--- file_handling_function/php_file_write.php
+++ file_handling_function/php_file_write.php
@@ -13,6 +13,8 @@ if ($file) {
     fwrite($file, $text2);
     $text3 = "In PHP, File handling is the process of interacting with files on the server, such as reading files, writing to a file, creating new files, or deleting existing ones.\n";
     fwrite($file, $text3);
+    $text3 = "Before you can read or write to a file, you need to open it using the fopen() function, which returns a file pointer resource. Once you're done working with the file, you should close it using fclose() to free up resources..\n";
+    fwrite($file, $text3);
     fclose($file); // Close the file
 } else {
     echo "Failed to open the file.";
