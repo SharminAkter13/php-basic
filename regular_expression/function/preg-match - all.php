4af90475c305f8e7965a74cdeb0153035c37baaf
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-05 12:28:56 +0600

    regex

--- regular_expression/function/preg-match - all.php
+++ regular_expression/function/preg-match - all.php
@@ -19,4 +19,31 @@ print_r($text);
 echo "<br>";
 echo "<br>";
 print_r($text1);
+
+
+
+echo "<br>";
+echo "<br>";
+
+$text2= "preg match all element testing 12435479698";
+$pattern ="/[0-7]/";
+echo preg_match_all($pattern,$text2);
+
+echo "<br>";
+echo "<br>";
+
+$text4= "leets eeeats leeeets geeeet going";
+$pattern2 ="/e{2,4}/i";
+echo preg_match_all($pattern2,$text4);
+
+
+echo "<br>";
+echo "<br>";
+
+$text5= "L4a";
+$pattern3 ="/^[a-zA-Z]{3,5}$/";
+echo preg_match_all($pattern3,$text5);
+
+
+
 ?>
