8e2695d24de426c023d6ae860b9d4b9e1313707d
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-08 12:14:21 +0600

    validation

--- validation/phone_email_validation.php
+++ validation/phone_email_validation.php
@@ -8,6 +8,8 @@
 </head>
 <body>
     <form method="post">
+        Contact:
+        <input type="text" name="con" ><br><br>
         Email:
         <input type="text" name="email" ><br><br>
         <input type="submit" name="submit" value="Submit">
@@ -15,12 +17,23 @@
 
     <?php
     if(isset($_POST['submit'])){
+        $con = $_POST['con'];
         $email = $_POST['email'];
-        $pattern ="/^[a-zA-Z0-9#%_\-\.]+[@][a-z]+[\.][a-z]{2,3}$/";
-
+        $phone_pattern ="/^[0-9+]{11,14}$/";
+        $email_pattern ="/^[a-zA-Z0-9#%_\-\.]+[@][a-z]+[\.][a-z]{2,3}$/";
         echo "<br>";
 
-        if(preg_match($pattern,$email)){
+        if(preg_match($phone_pattern,$con)){
+            echo "Phone Number is Valid";
+        }else{
+            echo "Phone Number is Invalid";
+
+        }
+
+        echo "<br>";
+        echo "<br>";
+
+        if(preg_match($email_pattern,$email)){
             echo "Email is Valid";
         }else{
             echo "Email is Invalid";
