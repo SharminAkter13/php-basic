d18ec49e9f4814b3066a1a3accea16182edc6ffb
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-09 10:36:16 +0600

    authentication

---
+++ authentication/main_page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+    <style>
+        body{
+            background-color: aliceblue;
+            text-align: center;
+            margin: 40px auto;
+            font-weight: bolder;
+        }
+        img{
+            width: 400px;
+            height: 400px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome To Our Landing Page</h1>
+    <div>
+        <img src="https://icon-library.com/images/desktop-computer-icon-png/desktop-computer-icon-png-1.jpg" alt="" >
+    </div>
+</body>
+</html>
