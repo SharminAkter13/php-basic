51e210a97894fd6f6b13d0e7f8fd87d6e883ffcb
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-12 13:11:21 +0600

    file upload

---
+++ file_upload/step-4-type.php
@@ -0,0 +1,66 @@
+<?php
+echo "<pre>";
+print_r($_FILES);
+echo "</pre>";
+    // line (2-5) not mendatory;
+
+if(isset($_POST['submit'])){
+    $file_add = $_FILES['file']['tmp_name'];
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_type = $_FILES['file']['type'];
+    $ok = 1;
+
+    $img = 'images/';
+    $kb =$file_size/1024;
+
+    // copy($tem_add, "images/".$file_name);
+    if($kb>400){
+        echo "File is Large";
+    }else{
+        move_uploaded_file($file_add,$img.$file_name);
+        echo "<b style='color: green;font-size: 16pt;'>Upload Success!!</b>";
+    }
+    if($file_type!="jpg" && $file_type!="Jpeg" && $file_type!="gif"){
+        echo " file not supported";
+        $ok = 0;
+    }
+
+   
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div style=" width:500px; margin:10px auto">
+    <form action="#" method="post" enctype="multipart/form-data">
+        <fieldset>
+            <input type="file" name="file" ><br><br>
+            <input type="submit" name="submit" >        
+            
+        </fieldset>
+    </form>
+    <?php
+    if(isset($_POST['submit'])){
+
+        echo "<div style='text-align:center;margin: auto'>
+        <img src='$img/$file_name' width='300px' style='margin-top: 30px'>
+      </div>";
+
+    //   echo "<b>File Name:</b> ".$_FILES['file']['name']."<br>";
+    //   echo "<b>Tmp Name:</b> ".$_FILES['file']['tmp_name']."<br>";
+    //   echo "<b>File Size:</b> ".$_FILES['file']['size']."<br>";
+    //   echo "<b>File Type:</b> ".$_FILES['file']['type']."<br>";
+    //   echo "<b>File Error:</b> ".$_FILES['file']['error']."<br>";
+    }
+
+    ?>
+    </div>
+</body>
+</html>
